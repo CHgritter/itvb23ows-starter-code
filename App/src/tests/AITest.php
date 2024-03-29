0fb653651ccad41af6a0aca914eec1dc6fbc9609
CHgritter <85736335+CHgritter@users.noreply.github.com>
2024-03-29 12:58:43 +0100

    Created unit tests for AI
    One test is to make sure that the info is retrieved correctly. The second is to make sure the move is made without any compromise.

--- App/src/tests/AITest.php
+++ App/src/tests/AITest.php
@@ -2,9 +2,52 @@
 
 namespace tests;
 
+use functions\Game;
+use functions\Database;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class AITest extends TestCase
 {
+    private $game;
+
+    protected function setUp(): void
+    {
+        // arrange
+        $dbMock = Mockery::mock(Database::class);
+        $dbMock->allows('newGame')->andReturns(1);
+        $dbMock->allows('placeMove')->andReturns(1);
+        $aiMock = Mockery::mock(AIHandler::class);
+        $aiMock->allows('aiCall')->andReturns(["play", "B", "1,0"]);
+        $this->game = new Game($dbMock, $aiMock);
+        $this->game->restart();
+        $_SESSION['board']['0,0'] = [[$_SESSION['player'], "Q"]];
+        $_SESSION['hand'][$this->game->getPlayer()]["Q"]--;
+        $_SESSION['player'] = 1 - $_SESSION['player'];
+        $_SESSION['board']['0,1'] = [[$_SESSION['player'], "Q"]];
+        $_SESSION['hand'][$this->game->getPlayer()]["Q"]--;
+        $_SESSION['player'] = 1 - $_SESSION['player'];
+    }
+
+    // Test one: make sure that AI gives a move.
+    public function testAICall() {
+        // act
+        $aiMove = $this->game->getAiMove();
+
+        // assert
+        self::assertNotNull($aiMove);
+    }
+
+    // Test two: make sure AI move gets played, regardless of it being legal or not.
+    public function testAIAction() {
+        // arrange
+        $aiMove = $this->game->getAiMove();
+
+        // act
+        $aiMove = $this->game->aiAction();
+
+        // assert
+        self::assertArrayHasKey('1,0', $this->game->getBoard());
+    }
 
 }
